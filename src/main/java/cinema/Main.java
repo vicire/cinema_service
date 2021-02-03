367a49e226ecddd2aa495342db1366a83b27972f
Roman Saviak <saviak.roman@gmail.com>
2021-02-03 20:57:23 +0200

    Make Shopping cart and ticket with dao and service

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -1,14 +1,15 @@
 package cinema;
 
-import cinema.exception.AuthenticationException;
 import cinema.lib.Injector;
 import cinema.model.CinemaHall;
 import cinema.model.Movie;
 import cinema.model.MovieSession;
+import cinema.model.User;
 import cinema.security.AuthenticationService;
 import cinema.service.CinemaHallService;
 import cinema.service.MovieService;
 import cinema.service.MovieSessionService;
+import cinema.service.ShoppingCartService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -42,12 +43,13 @@ public class Main {
 
         AuthenticationService service = (AuthenticationService) injector
                 .getInstance(AuthenticationService.class);
-        service.register("Human", "qwerty");
-        try {
-            System.out.println(service.login("Human", "qwerty"));
-            System.out.println(service.login("Human", "qwerty1"));
-        } catch (AuthenticationException e) {
-            System.out.println(e.getMessage());
-        }
+        User user = service.register("Human", "qwerty");
+        User user1 = service.register("Animal", "12345");
+        ShoppingCartService shoppingCartService = (ShoppingCartService) injector
+                .getInstance(ShoppingCartService.class);
+        shoppingCartService.addSession(movieSession, user);
+        System.out.println(shoppingCartService.getByUser(user));
+        shoppingCartService.addSession(movieSession, user1);
+        System.out.println(shoppingCartService.getByUser(user1));
     }
 }
---
+++ src/main/java/cinema/dao/TicketDaoImpl.java
@@ -0,0 +1,33 @@
+package cinema.dao;
+
+import cinema.exception.DataProcessingException;
+import cinema.lib.Dao;
+import cinema.model.Ticket;
+import cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class TicketDaoImpl implements TicketDao {
+    @Override
+    public Ticket add(Ticket ticket) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(ticket);
+            transaction.commit();
+            return ticket;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t add ticket " + ticket, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
