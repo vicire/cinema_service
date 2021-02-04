5d53700673f027dd71746e4e084b398211eef85e
Roman Saviak <saviak.roman@gmail.com>
2021-02-04 13:50:55 +0200

    Added Order and his dao and service

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -10,6 +10,7 @@ import cinema.security.AuthenticationService;
 import cinema.service.CinemaHallService;
 import cinema.service.MovieService;
 import cinema.service.MovieSessionService;
+import cinema.service.OrderService;
 import cinema.service.ShoppingCartService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -22,6 +23,10 @@ public class Main {
         movie.setTitle("Fast and Furious");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(movie);
+        Movie movie1 = new Movie();
+        movie1.setTitle("Lord of the rings");
+        movie1.setDescription("hobbits and elfs");
+        movieService.add(movie1);
         movieService.getAll().forEach(System.out::println);
 
         CinemaHall cinemaHall = new CinemaHall();
@@ -35,12 +40,17 @@ public class Main {
         MovieSession movieSession = new MovieSession();
         movieSession.setMovie(movie);
         movieSession.setCinemaHall(cinemaHall);
-        movieSession.setShowTime(LocalDateTime.of(2021, 2, 1, 20, 23));
+        movieSession.setShowTime(LocalDateTime.of(2021, 2, 10, 20, 23));
         MovieSessionService movieSessionService = (MovieSessionService) injector
                 .getInstance(MovieSessionService.class);
         movieSessionService.add(movieSession);
         movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now())
                 .forEach(System.out::println);
+        MovieSession movieSession1 = new MovieSession();
+        movieSession1.setMovie(movie1);
+        movieSession1.setCinemaHall(cinemaHall);
+        movieSession1.setShowTime(LocalDateTime.of(2021, 4, 4, 15, 45));
+        movieSessionService.add(movieSession1);
 
         AuthenticationService service = (AuthenticationService) injector
                 .getInstance(AuthenticationService.class);
@@ -54,6 +64,12 @@ public class Main {
         System.out.println(shoppingCartService.getByUser(user1));
         shoppingCartService.clear(shoppingCartService.getByUser(user1));
         System.out.println(shoppingCartService.getByUser(user1));
+
+        OrderService orderService = (OrderService) injector.getInstance(OrderService.class);
+        orderService.completeOrder(shoppingCartService.getByUser(user));
+        shoppingCartService.addSession(movieSession1, user);
+        System.out.println(orderService.completeOrder(shoppingCartService.getByUser(user)));
+        System.out.println(orderService.getOrderHistory(user));
         try {
             System.out.println(service.login("Human", "qwerty"));
             System.out.println(service.login("Human", "qwerty1"));
---
+++ src/main/java/cinema/service/OrderServiceImpl.java
@@ -0,0 +1,37 @@
+package cinema.service;
+
+import cinema.dao.OrderDao;
+import cinema.lib.Inject;
+import cinema.lib.Service;
+import cinema.model.Order;
+import cinema.model.ShoppingCart;
+import cinema.model.Ticket;
+import cinema.model.User;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order order = new Order();
+        List<Ticket> tickets = shoppingCart.getTickets();
+        order.setTickets(new ArrayList<>(tickets));
+        order.setUser(shoppingCart.getUser());
+        order.setOrderDate(LocalDateTime.now());
+        orderDao.add(order);
+        shoppingCartService.clear(shoppingCart);
+        return order;
+    }
+
+    @Override
+    public List<Order> getOrderHistory(User user) {
+        return orderDao.getOrderHistory(user);
+    }
+}
